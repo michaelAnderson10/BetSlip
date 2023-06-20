fede5bc7729131b05bc380002574b77847adebae
michaelAnderson10 <121829346+michaelAnderson10@users.noreply.github.com>
2023-06-20 17:00:58 +0100

    class UserInputManager.cs added. Process from Program.cs moved to UserInputManager.cs

--- BetSlip/Program.cs
+++ BetSlip/Program.cs
@@ -5,69 +5,12 @@ namespace BetSlip
 {
     internal class Program
     {
-
-        public static string? choice;
         static void Main(string[] args)
         {
 
-            List<Bet> bets = new List<Bet>();
+           
 
-            do
-            {
-                Console.WriteLine("Place a bet? (Y/N)");
-                choice = Console.ReadLine();
-                if (choice.Equals("Y", StringComparison.OrdinalIgnoreCase))
-                {                 
-
-                    Console.WriteLine("You chose to continue!");
-                  
-
-                    Console.Write("Enter Sport Event: ");
-                    string? SportEvent = Console.ReadLine();
-
-                    Console.Write("Enter Amount: ");
-                    string? AmountInput = Console.ReadLine();
-                    decimal Amount;
-                    if (decimal.TryParse(AmountInput, out Amount))
-                    {
-                        //
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Input");
-                    }
-
-                    Console.Write("Enter Odd: ");
-                    string? OddInput = Console.ReadLine();
-                    decimal Odd;
-                    if (decimal.TryParse(OddInput, out Odd))
-                    {
-                        //
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Input");
-                    }
-
-                    Bet bet = new Bet(SportEvent, Odd, Amount);
-                    bets.Add(bet);
-                    Console.WriteLine("Values Inputed");
-                    continue;
-
-                }
-                else if (choice.Equals("N", StringComparison.OrdinalIgnoreCase))
-                {                    
-                    Console.WriteLine("You chose to stop.");                  
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice. Please enter Y or N.");
-                }
-            }
-            while (!choice.Equals("N", StringComparison.OrdinalIgnoreCase));
-
-            Console.WriteLine("\nBet Slip");
-
+            List<Bet> bets = UserInputManager.UserInputConverter();
             BetProcessorMain betProcessor = new BetProcessorMain();
             List<BetReturn> betReturns = betProcessor.CalculateReturn(bets);
             BetOutcome outcome = betProcessor.CalculateTotalBetAndPotentialReturn(betReturns);
@@ -80,6 +23,7 @@ namespace BetSlip
             }
             Console.WriteLine($"Total Bet: £{betSlip.BetOutcome.TotalBet}");
             Console.WriteLine($"Total Potential Returns: £{betSlip.BetOutcome.TotalPotentialReturn}");
+
         }
     }
 }
---
+++ BetSlip/UserInputManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetSlip
+{
+    public class UserInputManager
+    {
+        public static string? choice;
+        public static List<Bet> UserInputConverter()
+        {
+            List<Bet> bets = new List<Bet>();
+
+            do
+            {
+                Console.WriteLine("Place a bet? (Y/N)");
+                choice = Console.ReadLine();
+                if (choice.Equals("Y", StringComparison.OrdinalIgnoreCase))
+                {
+
+                    Console.WriteLine("You chose to continue!");
+
+                    Console.Write("Enter Sport Event: ");
+
+                    string? SportEvent = Console.ReadLine();
+
+                    Console.Write("Enter Amount: ");
+                    string? AmountInput = Console.ReadLine();
+                    decimal Amount;
+                    if (decimal.TryParse(AmountInput, out Amount))
+                    {
+                        //
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+
+                    Console.Write("Enter Odd: ");
+                    string? OddInput = Console.ReadLine();
+                    decimal Odd;
+                    if (decimal.TryParse(OddInput, out Odd))
+                    {
+                        //
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input");
+                    }
+
+                    Bet bet = new Bet(SportEvent, Odd, Amount);
+                    bets.Add(bet);
+                    Console.WriteLine("Values Inputed");
+                    continue;
+
+                }
+                else if (choice.Equals("N", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Processing Bet Slip...");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter Y or N.");
+                }
+            }
+            while (!choice.Equals("N", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine("\nBet Slip");
+
+            return bets;
+
+        }
+
+    }
+}
