e1fae6f1ba1a9c9d95aaa5d44bc6af87a4c618d7
michaelAnderson10 <121829346+michaelAnderson10@users.noreply.github.com>
2023-06-20 15:52:29 +0100

    Unit test for CalculateTotalBetAndPotentialReturn_Checks() done

--- BetSlip/Test.cs
+++ BetSlip/Test.cs
@@ -16,20 +16,44 @@ namespace BetSlip
             };
             Bet footballBet = bets[0];
             BetReturn expectedFootballReturn = new BetReturn(footballBet.SportEvent, footballBet.Odd, footballBet.Amount, 30);
-
-
+                         
             //Act
             BetProcessorMain betProcessorMain = new BetProcessorMain();
             List<BetReturn> betReturns = betProcessorMain.CalculateReturn(bets);
 
             //Assert
-            Assert.AreEqual(2, betReturns.Count);
             BetReturn actualFootballReturn = betReturns[0];
 
+            Assert.AreEqual(2, betReturns.Count);
             Assert.AreEqual(expectedFootballReturn.SportEvent, actualFootballReturn.SportEvent);
             Assert.AreEqual(expectedFootballReturn.Odd, actualFootballReturn.Odd);
             Assert.AreEqual(expectedFootballReturn.Amount, actualFootballReturn.Amount);
             Assert.AreEqual(30, actualFootballReturn.PotentialReturns);
+        }
+
+        [Test]
+        public void CalculateTotalBetAndPotentialReturn_Checks()
+        {
+            //Arrange
+            List<BetReturn> betReturns = new List<BetReturn>
+            {
+                new BetReturn("Football", 2, 10, 30),
+                new BetReturn("Hockey", 2, 5, 15)
+            };
+            
+            decimal expectedTotalBet = 15;
+            decimal expectedTotalPotentialReturn = 45;
+            BetReturn football = betReturns[0];
+
+            //Act
+            BetProcessorMain betProcessorMain = new BetProcessorMain();
+            BetOutcome betOutcome = betProcessorMain.CalculateTotalBetAndPotentialReturn(betReturns);
+
+            //Assert
+            Assert.AreEqual(2, betReturns.Count());
+            Assert.AreEqual(expectedTotalBet, betOutcome.TotalBet);
+            Assert.AreEqual(expectedTotalPotentialReturn, betOutcome.TotalPotentialReturn);
+            
         }
     }
 }
